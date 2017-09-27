22adbd3ba8af8801891dd1eaa2cc667cce2fa882
Andrei Narkevich <NarkAndy@gmail.com>
2017-09-28 00:31:54 +0300

    brackets/feature: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   if (!str) {
+        return true;
+    }
+    for (let idx = 0; idx < str.length - 1; idx++) {
+        let pair = bracketsConfig.find(config => config[0] === str[idx]);
+        if (!pair) {
+            return false;
+        }
+        if (str[idx + 1] === pair[1]) {
+            str = str.replace(pair.join(''), '');
+            return check(str, bracketsConfig);
+        }
+    }
+    return false;
 }
